86c5b678f825f0d4b8950f758eb61104214db66d
PANKAJ KUMAR DAS <pankajkumardas8678@gmail.com>
2026-01-10 23:58:50 +0530

    Add Live Search Using PHP and AJAX

--- Live Search Using PHP and AJAX/load-search-data.php
+++ Live Search Using PHP and AJAX/load-search-data.php
@@ -4,8 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Live Search using PHP and AJAX</title>
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
 </head>
 <body>
-
+         <div class="row">
+            <div class="col-12">
+                <div class="table table">
+                    <thead>
+                        <th>ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Mohan</td>
+                                <td>Sharma</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Karan</td>
+                                <td>Gupta</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Ankit</td>
+                                <td>Pandy</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Saumya</td>
+                                <td>Chandan</td>
+                            </tr>
+                        </tbody>
+                    </thead>
+                </div>
+            </div>
+         </div>
 </body>
 </html>
